12e05bcf31897cd06e346346d46674cddaea934a
CDS15 <98981420+CDS15@users.noreply.github.com>
2022-03-11 18:12:42 -0500

    Trying to load the bike automatically.
    Trying to load the bike automatically.

--- src/init.js
+++ src/init.js
@@ -17,8 +17,12 @@ export default function init() {
     LinesProxy,
     Mesh,
     MeshProxy,
+    InstanceItem,
+    CADAsset,
+    CADBody,
+    PMIItem,
   } = zeaEngine
-  const { CADAsset, CADBody } = zeaCad
+  const { SelectionManager } = zeaUx
 
   const urlParams = new URLSearchParams(window.location.search)
   const scene = new Scene()
@@ -34,15 +38,23 @@ export default function init() {
   renderer.setScene(scene)
   renderer.getViewport().getCamera().setPositionAndTarget(new Vec3(12, 12, 10), new Vec3(0, 0, 1.5))
 
+  const envMap = new EnvMap()
+  envMap.load('./data/StudioG.zenv')
+  scene.setEnvMap(envMap)
+
+  const appData = {
+    scene,
+    renderer,
+  }
   /*
       Change the Background color
   */
-      const color = new Color('#7460e1') // this is equivalent to: new Color(116/255, 96/255, 225/255)
+/*       const color = new Color('#7460e1') // this is equivalent to: new Color(116/255, 96/255, 225/255)
       // get the settings of the scene.
       const settings = scene.getSettings()
       // get the "BackgroundColor" parameter and set the value to our color.
       settings.getParameter('BackgroundColor').setValue(color)
-          
+ */          
   /*
     Change Camera Manipulation mode
   */
@@ -51,9 +63,17 @@ export default function init() {
     .getManipulator()
     .setDefaultManipulationMode(CameraManipulator.MANIPULATION_MODES.turntable);
 
-  const envMap = new EnvMap();
-  envMap.load("../data/StudioG.zenv");
-  scene.setEnvMap(envMap);
+  // Setup Selection Manager
+  const selectionColor = new Color('#F9CE03')
+  selectionColor.a = 0.1
+  const selectionManager = new SelectionManager(appData, {
+    selectionOutlineColor: selectionColor,
+    branchSelectionOutlineColor: selectionColor,
+  })
+  appData.selectionManager = selectionManager
+  // Setup Progress Bar
+  const progressElement = document.getElementById('progress')
+  progressElement.resourceLoader = resourceLoader
 
   // Setup FPS Display
   const fpsElement = document.getElementById('fps')
@@ -64,13 +84,20 @@ export default function init() {
   treeElement.setTreeItem(scene.getRoot(), {
     scene,
     renderer,
+    selectionManager,
+    displayTreeComplexity: false,
   })
 
   // let highlightedItem
   const highlightColor = new Color('#F9CE03')
   highlightColor.a = 0.1
   const filterItem = (item) => {
-    while (item && !(item instanceof CADBody)) item = item.getOwner()
+    while (item && !(item instanceof CADBody) && !(item instanceof PMIItem)) {
+      item = item.getOwner()
+    }
+    if (item.getOwner() instanceof InstanceItem) {
+      item = item.getOwner()
+    }
     return item
   }
   renderer.getViewport().on('pointerDown', (event) => {
@@ -79,11 +106,11 @@ export default function init() {
       if (geomItem) {
         console.log(geomItem.getPath())
 
-        const geom = event.intersectionData.geomItem.getParameter('Geometry').getValue()
+        const geom = event.intersectionData.geomItem.geomParam.value
         console.log(geom.getNumVertices(), event.intersectionData.geomItem.geomIndex)
         let item = event.intersectionData.geomItem
         while (item) {
-          const globalXfo = item.getParameter('LocalXfo').getValue()
+          const globalXfo = item.localXfoParam.value
           console.log(item.getName(), globalXfo.sc.toString())
           item = item.getOwner()
         }
@@ -91,11 +118,26 @@ export default function init() {
     }
   })
 
-  resourceLoader.on('progressIncremented', (event) => {
-    const pct = document.getElementById('progress')
-    pct.value = event.percent
-    if (event.percent >= 100) {
-      setTimeout(() => pct.classList.add('hidden'), 1000)
+  renderer.getViewport().on('pointerUp', (event) => {
+    // Detect a right click
+    if (event.button == 0 && event.intersectionData) {
+      // // if the selection tool is active then do nothing, as it will
+      // // handle single click selection.s
+      // const toolStack = toolManager.toolStack
+      // if (toolStack[toolStack.length - 1] == selectionTool) return
+
+      // To provide a simple selection when the SelectionTool is not activated,
+      // we toggle selection on the item that is selcted.
+      const item = filterItem(event.intersectionData.geomItem)
+      if (item) {
+        if (!event.shiftKey) {
+          selectionManager.toggleItemSelection(item, !event.ctrlKey)
+        } else {
+          const items = new Set()
+          items.add(item)
+          selectionManager.deselectItems(items)
+        }
+      }
     }
   })
 
@@ -146,7 +188,7 @@ export default function init() {
     scene.getRoot().traverse((item) => {
       geomItems++
       if (item instanceof GeomItem) {
-        const geom = item.getParameter('Geometry').getValue()
+        const geom = item.geomParam.value
         if (geom instanceof Lines) {
           lines += geom.getNumSegments()
         } else if (geom instanceof LinesProxy) {
@@ -160,28 +202,39 @@ export default function init() {
     })
     console.log('geomItems:' + geomItems + ' lines: ' + lines + ' triangles:', triangles)
   }
-
-const loadCADAsset = (zcad, filename, framecamera) => {
-    const asset = new CADAsset(filename)
+  const loadCADAsset = (zcad, filename) => {
+    // Note: leave the asset name empty so that the asset
+    // gets the name of the product in the file.
+    const asset = new CADAsset()
 
     const context = new AssetLoadContext()
     // pass the camera in wth the AssetLoadContext so that
     // PMI classes can bind to it.
     context.camera = renderer.getViewport().getCamera()
     asset.load(zcad, context).then(() => {
-        if (framecamera)
-          renderer.getViewport().frameView([asset]);
-        //renderer.frameAll();
+      // The following is a quick hack to remove the black outlines around PMI text.
+      // We do not crete ourlines around transparent geometries, so by forcing
+      // the PMI items sub-trees to be considered transparent, it moves them into
+      // the GLTransparentPass, which does not draw outlines. this cleans up
+      // the rendering considerably.
+      asset.traverse((item) => {
+        if (item instanceof PMIItem) {
+          item.traverse((item) => {
+            if (item instanceof GeomItem) {
+              item.materialParam.value.__isTransparent = true
+            }
+          })
+          return false
+        }
+        return true
+      })
+
+      renderer.frameAll()
     })
     asset.getGeometryLibrary().on('loaded', () => {
       calcSceneComplexity()
     })
     scene.getRoot().addChild(asset)
- 
-    const xfo = new Xfo();
-    // xfo.ori.setFromEulerAngles(new EulerAngles(90 * (Math.PI / 180), 0, 0));
-    xfo.ori.setFromEulerAngles(new EulerAngles(180 * (Math.PI / 180), 90 * (Math.PI / 180), 0 * (Math.PI / 180))); //for PressRink
-    asset.getParameter("GlobalXfo").setValue(xfo);
  }
 
   const loadGLTFAsset = (url, filename) => {
@@ -218,17 +271,11 @@ const loadCADAsset = (zcad, filename, framecamera) => {
   }
 
   if (urlParams.has('zcad')) {
-    loadCADfile(urlParams.get('zcad'))
-    const dropZone = document.getElementById('dropZone')
-    if (dropZone) dropZone.hide()
+    loadCADAsset(urlParams.get('zcad'))
   } else if (urlParams.has('gltf')) {
     loadGLTFAsset(urlParams.get('gltf'))
-    const dropZone = document.getElementById('dropZone')
-    if (dropZone) dropZone.hide()
   } else if (urlParams.has('obj')) {
     loadOBJAsset(urlParams.get('obj'))
-    const dropZone = document.getElementById('dropZone')
-    if (dropZone) dropZone.hide()
   } else {
     const dropZone = document.getElementById('dropZone')
     dropZone.display((url, filename) => {
@@ -236,13 +283,12 @@ const loadCADAsset = (zcad, filename, framecamera) => {
     })
   }
 
-  //comment the following code if you want to load via URL params or the drop zone
-  if (dropZone) dropZone.hide()
-  //load default sample part
-  //loadCADAsset("./data/HC_SRO4.zcad", "HC_SRO4.zcad", true);
-
   //load default sample part
   loadCADAsset("./data/pinki/Full_na_stalcima.zcad", "Full_na_stalcima.zcad", true);
+
+  const xfo = new Xfo();
+  xfo.ori.setFromEulerAngles(new EulerAngles(90 * (Math.PI / 180), 0, 0));
+  asset.getParameter("GlobalXfo").setValue(xfo);
 
   //uncomment to load large automobile assembly
   // loadCADAsset("./data/01 dipan/01 dipan.zcad", "01 dipan.zcad", false);
